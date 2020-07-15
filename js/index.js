2adb9237c0e09bf6180539dd0ccc7b8e6bcb1d8f
Nico Thiebes <nico.thiebes@xing.com>
2020-07-15 09:46:50 +0200

    stuff

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
+// import { hallo, hi } from './test.js';
+
+// hallo('Samira');
+
+
+export default class Cat extends Animal {
+  constructor(name) {
+    this.name = name;
+    this.gender = 'f';
+  }
+
+  getGender() {
+    return this.gender;
+  }
+}
+
+const sami = new Human('Samira');
+const nico = new Human('Gscheid');
+
+console.log(sami.getGender());
+console.log(nico.getGender());
+
 console.log('♥️');
---
+++ js/test.js
@@ -0,0 +1,17 @@
+
+
+function hallo(name) {
+  console.log('hallo', name);
+}
+
+function hi(name) {
+  console.log('hi', name);
+}
+
+const body = document.querySelectorAll('body');
+
+
+export {
+  hallo,
+  hi
+};
